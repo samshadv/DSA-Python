72e4e75ed98653ac27cdc92ec0f26cae91d68028
samshadv <mohdsamshad1436@gmail.com>
2026-03-25 10:13:04 +0530

    Implement search function for rotated sorted array

---
+++ SearchinRotatedSortedArray.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def search(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return True
+
+           
+            if nums[left] == nums[mid] == nums[right]:
+                left += 1
+                right -= 1
+                continue
+
+           
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+        return False
+
+nums = [2, 5, 6, 0, 0, 1, 2]
+target = 0
+
+solution = Solution()
+result = solution.search(nums, target)
+
+print("Input: nums =", nums, ", target =", target)
+print("Output:", result)
