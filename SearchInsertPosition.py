941b3a78875afd9b56edbe3f86f8f8c925cab584
samshadv <mohdsamshad1436@gmail.com>
2026-03-26 12:00:24 +0530

    Implement searchInsert function to find the index of a target in a sorted array

---
+++ SearchInsertPosition.py
@@ -0,0 +1,28 @@
+#Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+#You must write an algorithm with O(log n) runtime complexity.
+
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        left = 0
+        right = len(nums)-1
+        
+        while left <= right:
+            mid = (left + right)//2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
+
+nums = [1,3,5,6]
+target = 5
+
+sol = Solution()
+output = sol.searchInsert(nums, target)
+
+print(output)
